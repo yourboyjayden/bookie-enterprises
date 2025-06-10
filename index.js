06fd55806f71caca3ae022b1ca5046180735f792
yourboyjayden <jthomasgamer1234@gmail.com>
2025-06-10 20:39:44 +1000

    Update index.js - get rid of !myrules in index.js and move command sequence to myrules.js

--- index.js
+++ index.js
@@ -37,16 +37,6 @@ Check out <#${importantInfoChannelId}> for important information, and make sure 
 client.on('messageCreate', message => {
   if (message.author.bot) return;
 
-  if (message.content.toLowerCase() === '!myrules') {
-    const embed = new EmbedBuilder()
-      .setTitle('Rules')
-      .setDescription('• No spamming\n• No mature content\n• No leaking Premium Bookie tips (If you are suspected, investigations will follow and your Premium Bookie account may be suspended)')
-      .setColor(0x1458f8) // 1333704 in hex
-      .setAuthor({ name: 'Bookie Enterprises - Important Information', iconURL: 'https://i.postimg.cc/PfzvMdPs/Bookie-Enterprises-Logo.png' })
-      .setFooter({ text: 'Rules are subject to change at any time' });
-
-    message.channel.send({ embeds: [embed] });
-  }
 
   if (message.content.toLowerCase() === '!verifying') {
     const embed = new EmbedBuilder()
